b5672ed959b0d871495a5105e42208d45920763f
floresb <44643065+floresb@users.noreply.github.com>
2023-12-20 08:13:20 -0600

    test1

---
+++ 2_automate_post.py
@@ -0,0 +1,17 @@
+from instabot import Bot
+
+# init bot
+bot = Bot()
+
+# login
+bot.login(username="", password="")
+
+# setup criteria to look for the posts and corresponding image
+
+# 1 create list of images
+
+# upload
+bot.upload_photo("photo.png", caption="Well Hi there!")
+
+# logout
+bot.logout()
